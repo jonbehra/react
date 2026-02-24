694faa1622861cb64fb85f5cd68025a6bf53897b
jonbehra <165320382+jonbehra@users.noreply.github.com>
2026-02-24 20:43:11 +0100

    .

--- lesson6.r/App.js
+++ lesson6.r/App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ButtonScreen from './screens/ButtonScreen';
+import MenuScreen from './screens/MenuScreen';
+
+
+const Stack = createStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Menu"
+        screenOptions={{
+          headerTitle: 'APP',
+        }}
+      >
+        <Stack.Screen name="Menu" component={MenuScreen} />
+        <Stack.Screen name="ButtonScreen" component={ButtonScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
